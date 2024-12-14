6ead15681748c14b10f98c01bc5fdf86af0c7751
Rishitha <trishithapandu2218@gmail.com>
2024-12-14 15:01:48 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Exception{
+    public static void main(String args[]){
+        try{
+            int d=90/0;
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Rishitha");
+        }
+    }
+}
